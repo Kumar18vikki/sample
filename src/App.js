a6f9bd40980f713a25329764b7965576da4990c5
kumar18vikki <vikkikumaran71@gmail.com>
2024-02-02 00:49:56 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+/*function App() {
+  let name = "abc";
+  return (
+    <div class="App">
+     <h1>Welcome</h1>
+     <h1>{name}</h1>
+      
+    </div>
+  );*/
+    /*<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+  }*/
+
+  function Form(){
+    const handleSubmit=(e) =>{
+
+    }
+    return(
+      <div>
+        <form onSubmit={e}</div>></form>
+      </div>
+    )
+      
+
+
+
+
+
+
+   
+    }
+
+
+export default App;
